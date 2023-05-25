a0990ac91935b6b05c695aee7ca16fe71b41d1d5
Sungwook Choi <sungwook.csw@gmail.com>
2023-05-25 21:10:12 +0900

    feat: apply Clerkprovider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
-import {type AppType} from "next/app";
+import { type AppType } from "next/app";
 
-import {api} from "~/utils/api";
-import {ClerkProvider} from "@clerk/nextjs";
+import { api } from "~/utils/api";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({Component, pageProps}) => {
-    return (<ClerkProvider {...pageProps}><Component {...pageProps} /></ClerkProvider>);
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
