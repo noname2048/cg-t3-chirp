ddf877ff60264152160774730d7ec26b58ac98a4
Sungwook Choi <sungwook.csw@gmail.com>
2023-05-30 02:38:06 +0900

    feat: add post create procedure in trpc

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,14 @@
 import type { User } from "@clerk/nextjs/dist/api";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs";
 import { TRPCError } from "@trpc/server";
+
+import { z } from "zod";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -42,4 +48,17 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
+  create: privateProcedure
+    .input(z.object({ content: z.string().emoji("Only emojis are allowed") }))
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
 });
